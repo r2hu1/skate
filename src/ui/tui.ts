25b3a5d2e0a34a17c804bd5bcc016c665cf14ae4
r2hu1 <newasram86@gmail.com>
2026-06-11 22:17:48 +0530

    Replace Ink UI with ora-based TUI spinner

---
+++ src/ui/tui.ts
@@ -0,0 +1,61 @@
+import ora, { type Ora } from "ora";
+import type { RankingResult } from "../types";
+
+class Tui {
+  private spinner: Ora | null = null;
+  private completedSteps: string[] = [];
+  private rankings: RankingResult[] = [];
+
+  startStep(step: string) {
+    if (this.spinner) {
+      this.spinner.succeed();
+    }
+    this.completedSteps.push(step);
+    this.spinner = ora(step).start();
+  }
+
+  log(msg: string) {
+    if (this.spinner) {
+      this.spinner.text = msg;
+    }
+  }
+
+  setRankings(rankings: RankingResult[]) {
+    this.rankings = rankings;
+  }
+
+  succeed(msg?: string) {
+    if (this.spinner) {
+      this.spinner.succeed(msg);
+      this.spinner = null;
+    }
+  }
+
+  fail(msg?: string) {
+    if (this.spinner) {
+      this.spinner.fail(msg);
+      this.spinner = null;
+    }
+  }
+
+  done(outputDir?: string) {
+    if (this.spinner) {
+      this.spinner.succeed();
+      this.spinner = null;
+    }
+    if (outputDir) {
+      console.log(`\n  Output: ${outputDir}/`);
+    }
+    console.log("\n Done!");
+  }
+
+  error(msg: string) {
+    if (this.spinner) {
+      this.spinner.fail(msg);
+      this.spinner = null;
+    }
+    console.error(`\n  Error: ${msg}`);
+  }
+}
+
+export const tui = new Tui();
